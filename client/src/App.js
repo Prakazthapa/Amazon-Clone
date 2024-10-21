4e92af0e2f3ad6545d9f5ccd614a7c9f67c4eb5e
PRAKASH THAPA <prakasambhav24@gmail.com>
2024-10-21 16:57:40 +0545

    all the frontend UI is completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import Footer from "./Components/footer/Footer.js";
 import Navbar from "./Components/header/Navbar.js";
 import MainComponent from "./Components/home/MainComponent.js";
 import Newnav from "./Components/newnavbar/Newnav.js";
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Newnav />
       <MainComponent />
+      <Footer />
     </>
   );
 }
